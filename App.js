d7518cec8299684d38dc74cc75bb214233c10837
Faiz e Rasool <75616203+Faiz4211@users.noreply.github.com>
2022-08-15 14:25:50 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+import TodoHeader from "./src/components/TodoHeader";
+import TodoList from "./src/components/TodoList";
+import store from "./src/redux/store";
+import { Provider } from "react-redux";
+
+export default function App() {
+    const RootApp = () => {
+        return (
+            <SafeAreaView>
+                <TodoHeader />
+                <TodoList />
+            </SafeAreaView>
+        );
+    };
+
+    return (
+        <Provider store={store}>
+            <RootApp />
+        </Provider>
+    );
+}
