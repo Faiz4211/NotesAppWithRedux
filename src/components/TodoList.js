411ae4d08bc62dc6840a80022eeff9ae28e8a910
Faiz e Rasool <75616203+Faiz4211@users.noreply.github.com>
2022-08-17 18:37:24 +0500

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,17 +4,25 @@ import {
     View,
     FlatList,
     TouchableOpacity,
-    ScrollView
+    ScrollView,
+    TextInput,
+    LogBox
 } from "react-native";
 import React from "react";
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { useSelector } from "react-redux";
 import { deleteTask } from "../redux/taskSlice";
+import { updateTodoTask } from "../redux/taskSlice";
 import { useDispatch } from "react-redux";
+
+
+
 
 const TodoList = () => {
     const dispatch = useDispatch();
     const todos = useSelector((state) => state.tasks);
+
+    // const edittodo = useSelector((state) => state.edittask);
     // console.log(todos);
     const data = [
         {
@@ -35,29 +43,47 @@ const TodoList = () => {
             })
         );
     };
+    const setInputEdit = (id) => {
+        dispatch(
+            updateTodoTask({
+                task: updateTodoTask,
+            })
+        )
+    }
 
-    //renderItem function with a delete button
+
+    // renderItem function with a delete button
     const renderItem = ({ item }) => {
         return (
+
             <ScrollView
-                horizontal={false}>
-                <View
-
-                    style={styles.item}>
-                    <Text style={styles.title}>{item.name}</Text>
+                horizontal={false}
+            >
+                <View style={styles.item}>
+                    <TextInput numberOfLines={4}
+                        style={styles.title}>{item.name}</TextInput>
                     <TouchableOpacity
-                        style={styles.deleteButton}
-                        onPress={() => onDelete(item.id)}
-                    >
+                        onPress={() => onDelete(item.id)}>
+                        <Icon style={styles.delete}
+                            name="delete" size={30} color="red" />
+                    </TouchableOpacity >
 
-                        <Icon name="delete" size={20} color="red" />
+                    <TouchableOpacity  >
+                        <Icon name="edit" size={30} color="green" />
                     </TouchableOpacity>
+
+                    <TouchableOpacity
+                        onPress={setInputEdit}>
+                        <Icon name="update" size={30} color="blue" />
+                    </TouchableOpacity>
+
                 </View >
-            </ScrollView>
+            </ScrollView >
         );
     };
 
     return (
+
         <View>
             <FlatList
                 data={todos}
@@ -65,26 +91,25 @@ const TodoList = () => {
                 keyExtractor={(item) => item.id}
             />
         </View>
+
+
     );
 };
-
 
 const styles = StyleSheet.create({
     item: {
         backgroundColor: "#e9e9e9",
-        padding: 20,
+        marginTop: 20,
         marginVertical: 8,
         marginHorizontal: 16,
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
+
     },
     title: {
         fontSize: 20,
-    },
-    delete: {
-        fontSize: 24,
-        color: "red",
+
     },
 });
 
